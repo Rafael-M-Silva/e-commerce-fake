032567ecba799fc1f8317183c0fdac7014a834c4
Rafael Mauricio <rafael_mauricio@hotmail.com>
2023-10-19 15:11:48 -0300

    pagina vermais ok

--- js/script.js
+++ js/script.js
@@ -10,18 +10,30 @@ const displayProduct = (data)=> {
     const products = document.querySelector('.products')
     
     data.forEach((data) => {
-        const { title, price, image } = data
+        const { title, price, image, id } = data
         const productDiv = document.createElement('div')
         productDiv.classList.add('product')
         
         productDiv.innerHTML = `
             <img src="${image}" alt="">
-            <p>${title}</p>
+            <p class="id-produto" id=${id}>${title}</p>
             <span>R$${price}</span>
             <div class="btn">
             <button>Comprar</button>
             <button class='btn-produto'>Ver mais</button>
         `
         products.appendChild(productDiv)
+        const btnViewMore = document.querySelectorAll('.btn-produto')
+        for(var i = 0; i < btnViewMore.length; i++) {
+            btnViewMore[i].addEventListener('click', viewMore)
+        }        
     });
+}
+
+const viewMore = (event) => {
+    const product = event.target.parentElement.parentElement
+    const productId = product.querySelector('.id-produto')
+    console.log(productId.id);
+
+    window.location.href = `./produto.html?id=${productId.id}`
 }
