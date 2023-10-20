9c8d6e74734cfc2013a49eb82d2da19d3f7c7989
Rafael Mauricio da SIlva <rafael_mauricio@hotmail.com>
2023-10-19 21:20:10 -0300

    buscando por categoria

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const API = `https://fakestoreapi.com/products`
+let API = `https://fakestoreapi.com/products`
 
 fetch(API)
 .then((res)=> res.json())
@@ -36,4 +36,50 @@ const viewMore = (event) => {
     console.log(productId.id);
 
     window.location.href = `./produto.html?id=${productId.id}`
+}
+
+
+const buscarCategoria = (event) => {
+    let categorie = event.target.innerHTML
+    let linkCategorie = `/category/${categorie}`
+
+    const API = `https://fakestoreapi.com/products${linkCategorie}`
+    
+    fetch(API)
+    .then((res)=> res.json())
+    .then(data => {
+        displayProduct(data)
+})
+
+const displayProduct = (data)=> {
+    document.querySelector('.products').innerHTML = ''
+    const products = document.querySelector('.products')
+    
+    data.forEach((data) => {
+        const { title, price, image, id } = data
+
+        const productDiv = document.createElement('div')
+        productDiv.classList.add('product')
+        
+        productDiv.innerHTML = `
+            <img src="${image}" alt="">
+            <p class="id-produto" id=${id}>${title}</p>
+            <span>R$${price}</span>
+            <div class="btn">
+            <button>Comprar</button>
+            <button class='btn-produto'>Ver mais</button>
+        `
+        products.appendChild(productDiv)
+        const btnViewMore = document.querySelectorAll('.btn-produto')
+        for(var i = 0; i < btnViewMore.length; i++) {
+            btnViewMore[i].addEventListener('click', viewMore)
+        }        
+    });
+}
+
+}
+
+const btnCategories = document.querySelectorAll('nav ul li a')
+for(var i = 0; i < btnCategories.length; i++) {
+    btnCategories[i].addEventListener('click', buscarCategoria)
 }
