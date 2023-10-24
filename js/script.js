c74e56db4bc04ba21513d4b6340fc0e6f47fec21
Rafael Mauricio <rafael_mauricio@hotmail.com>
2023-10-24 14:43:59 -0300

    fix: bug voltar para home

--- js/script.js
+++ js/script.js
@@ -1,47 +1,52 @@
-let API = `https://fakestoreapi.com/products`
+const home = () => {
+    document.querySelector('.products').innerHTML = ''
+    document.querySelector('.title h1').innerHTML = `PRODUCTS`
+    let API = `https://fakestoreapi.com/products`
 
-fetch(API)
-.then((res)=> res.json())
-.then(data => {
-    displayProduct(data)
-})
-
-const displayProduct = (data)=> {
-    const products = document.querySelector('.products')
+    fetch(API)
+    .then((res)=> res.json())
+    .then(data => {
+        displayProduct(data)
+    })
     
-    data.forEach((data) => {
-        const { title, price, image, id } = data
-        const productDiv = document.createElement('div')
-        productDiv.classList.add('product')
+    const displayProduct = (data)=> {
+        const products = document.querySelector('.products')
         
-        productDiv.innerHTML = `
-            <img src="${image}" alt="">
-            <p class="id-produto" id=${id}>${title}</p>
-            <span>R$${price}</span>
-            <div class="btn">
-            <button>Comprar</button>
-            <button class='btn-produto'>Ver mais</button>
-        `
-        products.appendChild(productDiv)
-        const btnViewMore = document.querySelectorAll('.btn-produto')
-        for(var i = 0; i < btnViewMore.length; i++) {
-            btnViewMore[i].addEventListener('click', viewMore)
-        }        
-    });
+        data.forEach((data) => {
+            const { title, price, image, id } = data
+            const productDiv = document.createElement('div')
+            productDiv.classList.add('product')
+            
+            productDiv.innerHTML = `
+                <img src="${image}" alt="">
+                <p class="id-produto" id=${id}>${title}</p>
+                <span>R$${price}</span>
+                <div class="btn">
+                <button>Comprar</button>
+                <button class='btn-produto'>Ver mais</button>
+            `
+            products.appendChild(productDiv)
+            const btnViewMore = document.querySelectorAll('.btn-produto')
+            for(var i = 0; i < btnViewMore.length; i++) {
+                btnViewMore[i].addEventListener('click', viewMore)
+            }        
+        });
+    }
+    
 }
+home()
 
 const viewMore = (event) => {
     const product = event.target.parentElement.parentElement
     const productId = product.querySelector('.id-produto')
-    console.log(productId.id);
 
     window.location.href = `./produto.html?id=${productId.id}`
 }
 
-
 const buscarCategoria = (event) => {
     let categorie = event.target.innerHTML
     let linkCategorie = `/category/${categorie}`
+    document.querySelector('.title h1').innerHTML = `${categorie}`
 
     const API = `https://fakestoreapi.com/products${linkCategorie}`
     
@@ -49,7 +54,7 @@ const buscarCategoria = (event) => {
     .then((res)=> res.json())
     .then(data => {
         displayProduct(data)
-})
+    })
 
 const displayProduct = (data)=> {
     document.querySelector('.products').innerHTML = ''
@@ -76,10 +81,26 @@ const displayProduct = (data)=> {
         }        
     });
 }
-
 }
 
 const btnCategories = document.querySelectorAll('nav ul li a')
 for(var i = 0; i < btnCategories.length; i++) {
     btnCategories[i].addEventListener('click', buscarCategoria)
 }
+
+const voltarHome = document.querySelector('.home')
+voltarHome.addEventListener('click', home)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
